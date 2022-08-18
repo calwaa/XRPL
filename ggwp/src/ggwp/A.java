8fa50cf1b3a4e5ddf9a9735d5940e8cf9aa2be88
calwaa <111478695+calwaa@users.noreply.github.com>
2022-08-18 11:05:37 +0700

    XR3

---
+++ ggwp/src/ggwp/A.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ggwp;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class A {
+    String s = "Class A";
+}
---
+++ ggwp/src/ggwp/MainClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ggwp;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        B C = new B();
+        System.out.println(C.s);
+    }
+}
